832cb308ff6e054c6b43c8a7879e4375a641b64f
dp-devs <digiprayas@gmail.com>
2023-02-07 23:09:29 +0530

    vercel-added

--- DigiprayasAPI/settings.py
+++ DigiprayasAPI/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-94g=o9#j)$)-maqv$s37o8xdam^*9k^1ar*76)^v=w%*a__wkp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh','127.0.0.1']
 
 
 # Application definition
--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,3 @@
 # build_files.sh
 pip install -r requirements.txt
-pip install --root-user-action=ignore
-pip install --upgrade --no-binary hdbscan hdbscan
 python3.9 manage.py collectstatic
