d37b2809e83de9cea26db8191b55d34d74cc3a8a
dp-devs <digiprayas@gmail.com>
2023-02-07 23:02:53 +0530

    vercel-added

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 # build_files.sh
 pip install -r requirements.txt
-pip install --upgrade pip
 pip install --root-user-action=ignore
+pip install --upgrade --no-binary hdbscan hdbscan
 python3.9 manage.py collectstatic
