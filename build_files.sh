2b5ed902b314b085cf94abb870569067989a26c2
dp-devs <digiprayas@gmail.com>
2023-02-07 23:36:30 +0530

    vercel

--- build_files.sh
+++ build_files.sh
@@ -1,13 +1,6 @@
-# build_files.sh
-pip install -r requirements.txt
-pip install --root-user-action=ignore requests
-pip install --upgrade pip
-pip install pymysql
-pip install --upgrade setuptools
 pip install django
 pip install djangorestframework
 pip install django-cors-headers
 pip install mysqlclient
-pip install psycopg2
-pip install pyodbc
+pip install -r requirements.txt
 python3.9 manage.py collectstatic
