2ae997ff5be429ea0ed1b13f76fad8b40c376aa7
dp-devs <digiprayas@gmail.com>
2023-02-07 22:20:33 +0530

    vercel-update

--- EmployeeApp/urls.py
+++ EmployeeApp/urls.py
@@ -10,3 +10,6 @@ urlpatterns=[
     url(r'^employee$',views.employeeApi),
     url(r'^employee/([0-9]+)$',views.employeeApi),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
