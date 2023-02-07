5fc0c35709f317d949c2846b16c25377762dec8b
dp-devs <digiprayas@gmail.com>
2023-02-07 21:55:51 +0530

    vercel-added

--- DigiprayasAPI/settings.py
+++ DigiprayasAPI/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-94g=o9#j)$)-maqv$s37o8xdam^*9k^1ar*76)^v=w%*a__wkp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -141,3 +141,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
