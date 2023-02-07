391b03100c7e892afbac408dafdeba6433a4410f
dp-devs <digiprayas@gmail.com>
2023-02-08 00:10:52 +0530

    vercel

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,5 @@
 pip install -r requirements.txt
-pip install mysql-python
-pip install -U setuptools
+pip install mysqlclient
 pip install django
 pip install djangorestframework
 pip install django-cors-headers
