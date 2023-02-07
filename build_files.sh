0c6ccb178e14453a088302a28b2b9f5e53252a51
dp-devs <digiprayas@gmail.com>
2023-02-07 23:44:15 +0530

    vercel

--- build_files.sh
+++ build_files.sh
@@ -2,6 +2,5 @@ pip install pip==21.3.1
 pip install django
 pip install djangorestframework
 pip install django-cors-headers
-pip install psycopg2
 pip install -r requirements.txt
 python3.9 manage.py collectstatic
