97ccb4991b0d63a92084db2165a3aee3b38da0a2
dp-devs <digiprayas@gmail.com>
2023-02-08 12:01:08 +0530

    railway-added

--- DigiprayasAPI/settings.py
+++ DigiprayasAPI/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-94g=o9#j)$)-maqv$s37o8xdam^*9k^1ar*76)^v=w%*a__wkp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*','127.0.0.1']
+ALLOWED_HOSTS = ['*','127.0.0.1','python-employee-production.up.railway.app']
 
 
 # Application definition
